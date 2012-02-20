0505b06eb0c5686bdde6fb2cbba7e264d08dc473
Dan Gilbert <dgilbert@groupon.com>
2012-02-20 11:11:43 -0600

    Working prototype.

---
+++ backbone_named_routes.js
@@ -0,0 +1,55 @@
+//     Backbone Named Routes 0.1.0
+//     (c) 2012 Groupon Inc.
+
+Backbone.NamedRoutes = {
+  PATTERNS: {},
+
+  pathFor: function(pathPattern, urlParams, queryParams) {
+    var path = pathPattern;
+    if (path[0] !== "/") path = "/" + path;
+    for(var i = 0; i < urlParams.length; i++) {
+      var param = urlParams[i];
+      path = path.replace(/\:\w+/, param);
+    }
+    if (queryParams) {
+      path += "?" + $.param(queryParams);
+    }
+    return path;
+  }
+};
+
+_(Backbone.View.prototype).extend({
+  helper: Backbone.NamedRoutes
+});
+
+_(Backbone.Router.prototype).extend({
+  helper: Backbone.NamedRoutes
+});
+
+_(Backbone.Router.prototype).extend({
+  route: (function(original) {
+    return function(route, name, callback) {
+      // Create key to store path patterns for this route name.
+      Backbone.NamedRoutes.PATTERNS[name] = Backbone.NamedRoutes.PATTERNS[name] || {};
+
+      // Store the route pattern for the combination of this route name and the
+      // number of path params defined in the pattern.
+      var numberOfParams = route.match(/\:\w+/g) ? route.match(/\:\w+/g).length : 0;
+      Backbone.NamedRoutes.PATTERNS[name][numberOfParams] = route;
+
+      // Create the named route helper method for this route name.
+      Backbone.NamedRoutes[name + 'Path'] = function() {
+        var args = Array.prototype.slice.call(arguments);
+        var hasQueryParams = _(args[args.length-1]).isObject();
+        var numberOfParams = hasQueryParams ? arguments.length - 1 : arguments.length;
+        var routePattern = Backbone.NamedRoutes.PATTERNS[name][numberOfParams];
+        var queryParams = hasQueryParams ? args.pop() : null;
+
+        return Backbone.NamedRoutes.pathFor(routePattern, args, queryParams);
+      };
+
+      // Invoke the orginal #route method.
+      original.apply(this, arguments);
+    };
+  }(Backbone.Router.prototype.route))
+});
---
+++ spec/backbone_named_routes_spec.js
@@ -0,0 +1,62 @@
+describe("Backbone named routes extension", function() {
+
+  var Workspace = Backbone.Router.extend({
+    routes: {
+      "help":                 "help",    // #help
+      "search/:query":        "search",  // #search/kiwis
+      "search/:query/p:page": "search"   // #search/kiwis/p7
+    }
+  });
+
+  describe("Views", function() {
+    it("provides a helper with named routes", function() {
+      var router = new Workspace();
+      var view = new Backbone.View();
+
+      expect(view.helper.helpPath()).toEqual("/help");
+      expect(view.helper.searchPath('kiwis')).toEqual("/search/kiwis");
+      expect(view.helper.searchPath('kiwis', 7)).toEqual("/search/kiwis/p7");
+    });
+
+    it("allows arbitrary query params", function() {
+      var router = new Workspace();
+      var view = new Backbone.View();
+
+      expect(view.helper.helpPath({ foo: "bar", baz: "boo" })).toEqual("/help?foo=bar&baz=boo");
+      expect(view.helper.searchPath('kiwis', { foo: "bar", baz: "boo" })).toEqual("/search/kiwis?foo=bar&baz=boo");
+      expect(view.helper.searchPath('kiwis', 7, { foo: "bar", baz: "boo" })).toEqual("/search/kiwis/p7?foo=bar&baz=boo");
+    });
+  });
+
+  describe("Routers", function() {
+    it("provides a helper with named routes", function() {
+      var router = new Workspace();
+
+      expect(router.helper.helpPath()).toEqual("/help");
+      expect(router.helper.searchPath('kiwis')).toEqual("/search/kiwis");
+      expect(router.helper.searchPath('kiwis', 7)).toEqual("/search/kiwis/p7");
+    });
+
+    it("allows arbitrary query params", function() {
+      var router = new Workspace();
+      var view = new Backbone.View();
+
+      expect(router.helper.helpPath({ foo: "bar", baz: "boo" })).toEqual("/help?foo=bar&baz=boo");
+      expect(router.helper.searchPath('kiwis', { foo: "bar", baz: "boo" })).toEqual("/search/kiwis?foo=bar&baz=boo");
+      expect(router.helper.searchPath('kiwis', 7, { foo: "bar", baz: "boo" })).toEqual("/search/kiwis/p7?foo=bar&baz=boo");
+    });
+  });
+
+  it("allows implementors to mix in the url helper into other places", function() {
+    _(Backbone.Model.prototype).extend({
+      helper: Backbone.NamedRoutes
+    });
+
+    var router = new Workspace();
+    var model = new Backbone.Model();
+
+    expect(model.helper.helpPath()).toEqual("/help");
+    expect(model.helper.searchPath('kiwis')).toEqual("/search/kiwis");
+    expect(model.helper.searchPath('kiwis', 7)).toEqual("/search/kiwis/p7");
+  });
+});
